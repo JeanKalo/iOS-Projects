b4b6fc70f60eab001c4f88920c62f0c5ba73b111
Jean Carlos Quejada Toro <jean.quejada@sioma.app>
2022-02-25 11:25:36 -0500

    RFACTOR: quito el List y lo cambio por un LzayVgrid

--- InfinitiveScroll/InfinitiveScroll/ContentView.swift
+++ InfinitiveScroll/InfinitiveScroll/ContentView.swift
@@ -13,58 +13,51 @@ struct ContentView: View {
         
         
         ScrollViewReader { reader in
-            List {
-                ForEach(0...dataVm.data.count,id:\.self) { x in
-                    //Valida si ya llego al top y debe mostrar el loading
-                    if x == dataVm.data.count {
-                        HStack {
-                            if !dataVm.couldRefreshMore {
-                                Text("Actualizado hasta la fecha actual")
-                                    .padding(10)
-                                    .font(.footnote)
-                                    .background(Color.yellow)
-                                    .onTapGesture {
-                                        print("RESET DATA")
-                                        dataVm.resetData()
-                                        withAnimation {
-                                            reader.scrollTo(0, anchor: .top)
+            ScrollView {
+                LazyVGrid(columns:[GridItem(.flexible())]) {
+                    ForEach(0...dataVm.data.count,id:\.self) { x in
+                        //Valida si ya llego al top y debe mostrar el loading
+                        if x == dataVm.data.count {
+                            HStack {
+                                if !dataVm.couldRefreshMore {
+                                    Text("Actualizado hasta la fecha actual")
+                                        .padding(10)
+                                        .font(.footnote)
+                                        .background(Color.yellow)
+                                        .onTapGesture {
+                                            print("RESET DATA")
+                                            dataVm.resetData()
+                                            withAnimation {
+                                                reader.scrollTo(0, anchor: .top)
+                                            }
+                                            
                                         }
-                                        
-                                    }
-                            }else{
-                                ProgressView()
-                                    .onAppear {
-                                        if dataVm.couldRefreshMore && !dataVm.isResetingData {
-                                            print("Load more data")
-                                            dataVm.loadMoreData()
-                                        }else{
-                                            print("LLegamos al final \(dataVm.numberOfPack*dataVm.pack)")
+                                }else{
+                                    ProgressView()
+                                        .onAppear {
+                                            if dataVm.couldRefreshMore && !dataVm.isResetingData {
+                                                print("Load more data")
+                                                dataVm.loadMoreData()
+                                            }else{
+                                                print("LLegamos al final \(dataVm.numberOfPack*dataVm.pack)")
+                                            }
                                         }
-                                    }
+                                }
                             }
-                        }
-                        .padding(50)
-                        .frame(maxWidth: .infinity)
-                        .listRowInsets(.none)
-                        .listRowSeparator(.hidden)
-                        .listRowBackground(Color.white)
-                        .listSectionSeparator(.hidden)
-                        
-                    }
-                    else {
-                        Text(dataVm.data[x].description)
                             .padding(50)
                             .frame(maxWidth: .infinity)
-                            .background(Color.gray.opacity(0.2))
-                            .listRowInsets(.none)
-                            .listRowSeparator(.hidden)
-                            .listRowBackground(Color.white)
-                            .listSectionSeparator(.hidden)
-                            .id(x)
+                            
+                        }
+                        else {
+                            Text(dataVm.data[x].description)
+                                .padding(50)
+                                .frame(maxWidth: .infinity,maxHeight: 400)
+                                .background(Color.gray.opacity(0.2))
+                                .id(x)
+                        }
                     }
                 }
             }
-            .listStyle(.plain)
         }
         
         
